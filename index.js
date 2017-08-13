fcf3693e8a3a0dc627915f12181d76bce97c9da1
gilk <gilktest@gilktest.com>
2017-08-13 11:44:58 +0300

    finished for now with oauth + nodemon

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
@@ -9,12 +10,26 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callback: '/auth/google/callback'
+      callbackURL: '/auth/google/callback'
     },
-    accessToken => {
-      console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token', accessToken);
+      console.log('refresh token', refreshToken);
+      console.log('profile', profile);
     }
   )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google')
 );
 
 const PORT = process.env.PORT || 5000;
