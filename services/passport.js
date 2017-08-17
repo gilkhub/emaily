ac2b5ae1a012f49e418e06f469ddde8796d8f69e
gilk <gilktest@gilktest.com>
2017-08-17 11:26:12 +0300

    refactored google strategy to async syntax

--- services/passport.js
+++ services/passport.js
@@ -23,18 +23,15 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // this user already exists. don't create a new user.
-          done(null, existingUser);
-        } else {
-          // New user, save it!
-          new User({ googleId: profile.id })
-          .save()
-          .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      user = await new User({ googleId: profile.id }).save();
+      user => done(null, user);
     }
   )
 );
