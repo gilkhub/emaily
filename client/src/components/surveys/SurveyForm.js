fc0985941ca1be560f05112c73af90fd12f07602
gilk <gilktest@gilktest.com>
2017-08-31 00:24:05 +0300

    11.164 done

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -3,31 +3,26 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
-
-const FIELDS = [
-  { label: 'Survey title', name: 'title'},
-  { label: 'Subject line', name: 'subject'},
-  { label: 'Email body', name: 'body'},
-  { label: 'Recipient list', name: 'emails'}
-];
+import validateEmails from '../../utils/validateEmails';
+import formFields from './formFields';
 
 class SurveyForm extends Component {
   renderFields() {
-    return _.map(FIELDS, ({label, name}) => {
+    return _.map(formFields, ({label, name}) => {
       return <Field key={name} component={SurveyField} type="text" label={label} name={name} />
     });
   }
 
-  render(){
+  render() {
     return (
       <div>
-        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+        <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
           {this.renderFields()}
           <Link to="/surveys" className="red btn-flat left white-text">
             Cancel
           </Link>
           <button type="submit" className="teal btn-flat right white-text">
-            Submit
+            Next
             <i className="material-icons right">done</i>
           </button>
         </form>
@@ -36,6 +31,22 @@ class SurveyForm extends Component {
   }
 }
 
+function validate(values) { // Why not validate(values){}? why must specify 'function'?
+  const errors = {};
+
+  errors.emails = validateEmails(values.emails || '');
+
+  _.each(formFields, ({ name }) => {
+    if (!values[name]) {
+      errors[name] = 'You must provide a value';
+    }
+  });
+
+  return errors;
+}
+
 export default reduxForm({
-  form: 'surveyForm'
+  validate,
+  form: 'surveyForm',
+  destroyOnUnmount: false
 })(SurveyForm);
